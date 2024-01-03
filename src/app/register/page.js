219fbd7255fdb814b04cd0eac65c0d0c1e4db973
Nikhith Nukala <nikhithnukala@Nikkis-MacBook-Air.local>
2024-01-03 12:14:22 +0530

    header footer changes

---
+++ src/app/register/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './register.module.css'
+
+const Register = () => {
+  return (
+    <div className={style.clr}>
+      register
+    </div>
+  )
+}
+
+export default Register
